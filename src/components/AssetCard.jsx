b383ed0bb51d7908c4e258eb8d700a3c321530cd
saverio mandarano <saverio.mandarano1@gmail.com>
2026-06-16 12:59:22 +0200

    Rendering assets list in HomePage

---
+++ src/components/AssetCard.jsx
@@ -0,0 +1,26 @@
+// export default function AssetCard(props) {
+// const { id, description, image_url, name, ticker } = props.assetProp;
+// }
+
+import { Link } from "react-router-dom";
+
+export default function AssetCard({ assetProp }) {
+  const { id, description, image_url, name, ticker } = assetProp;
+
+  return (
+    <Link to={`assets/${id}`}>
+      <div className="card h-100">
+        <h6 className="text-muted ">{ticker}</h6>
+        <div className="card-img-wrapper">
+          <img src={image_url} className="card-img-top" alt={name} />
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">
+            <i>{description}</i>
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,53 @@
+import AssetCard from "../components/AssetCard";
+
 export default function HomePage() {
-  return <div>Assets List</div>;
+  const assets = [
+    {
+      id: 1,
+      category_id: 4,
+      lastPrice: 228.5,
+      description: "Big tech hardware e servizi digitali",
+      image_url: "/images/apple.png",
+      name: "Apple Inc.",
+      ticker: "AAPL",
+    },
+    {
+      id: 2,
+      category_id: 4,
+      lastPrice: 415.3,
+      description: "Software enterprise e cloud computing",
+      image_url: "/images/microsoft.png",
+      name: "Microsoft Corporation",
+      ticker: "MSFT",
+    },
+    {
+      id: 3,
+      category_id: 4,
+      lastPrice: 875.28,
+      description: "GPU e infrastrutture AI",
+      image_url: "/images/nvidia.png",
+      name: "Nvidia Corporation",
+      ticker: "NVDA",
+    },
+    {
+      id: 4,
+      category_id: 4,
+      lastPrice: 140.8,
+      description: "Search, advertising e AI",
+      image_url: "/images/google.png",
+      name: "Google / Alphabet Inc.",
+      ticker: "GOOGL",
+    },
+  ];
+  return (
+    <div className="container my-5">
+      <div className="row g-4">
+        {assets.map((asset) => (
+          <div key={asset.id} className=" col-12 col-md-6 col-lg-4">
+            <AssetCard assetProp={asset} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
