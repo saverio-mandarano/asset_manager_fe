87303e9b9f159d1e458a1be251cca25ed9958561
saverio mandarano <saverio.mandarano1@gmail.com>
2026-06-16 19:56:46 +0200

    Add AssetDetailPage.jsx card

--- src/pages/AssetDetailsPage.jsx
+++ src/pages/AssetDetailsPage.jsx
@@ -23,33 +23,9 @@ export default function AssetDetailsPage() {
     <div className="container">
       {asset ? (
         <>
-          <section id="asset-details" className="card my-4 w-100">
+          <section id="asset-details" className="card my-4">
             <AssetDetailsPageCard assetProp={asset} />
           </section>
-
-          {/* search bar (se hai una lista associata, es. news / transactions / holdings) */}
-          {/* <div className="input-group mb-3 mt-5">
-            <input
-              onChange={(e) => setInputData(e.target.value)}
-              type="text"
-              className="form-control"
-              placeholder="Search..."
-            />
-
-            <button className="btn btn-success" type="button">
-              Cerca
-            </button>
-          </div> */}
-
-          {/* lista filtrata (esempio: holdings / related items / transactions) */}
-          {/* <div className="accordion mb-5" id="assetAccordion">
-            {filteredData.map((item) => (
-              <div key={item.id}> */}
-          {/* sostituisci con il tuo componente reale */}
-          {/* <div className="card p-2 mb-2">{item.name}</div>
-              </div>
-            ))}
-          </div> */}
         </>
       ) : (
         <div>Loading...</div>
