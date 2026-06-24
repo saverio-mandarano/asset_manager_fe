5fcabc91ef8c6d1c38efb4d441c1fd97be79901d
saverio mandarano <saverio.mandarano1@gmail.com>
2026-06-25 00:43:40 +0200

    Implement searchbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,17 +46,6 @@ const Header = () => {
                 </NavLink>
               </li>
             </ul>
-            <form className="d-flex pe-4" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-primary" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,20 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 export default function HomePage() {
+  const [query, setQuery] = useState("");
+  const [submittedQuery, setSubmittedQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedQuery(query);
+  };
+
   const [assets, setAssets] = useState([]);
-  const endpoint = "http://localhost:8080/api/assets";
 
   useEffect(() => {
+    const endpoint = submittedQuery
+      ? `http://localhost:8080/api/assets?name=${submittedQuery}`
+      : `http://localhost:8080/api/assets`;
     axios
       .get(endpoint)
       .then((res) => {
@@ -16,10 +26,28 @@ export default function HomePage() {
       .catch((err) => {
         console.log("error fetch assets: ", err.message);
       });
-  }, []);
+  }, [submittedQuery]);
 
   return (
     <>
+      {/* searchbar */}
+      <div className="row mb-4">
+        <div className="col-12 col-md-6 col-lg-4 ms-auto">
+          <form className="d-flex" onSubmit={handleSubmit} role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Name or Ticker..."
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button className="btn btn-outline-primary" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+
       <div className="row g-4">
         {assets.map((asset) => (
           <div key={asset.id} className=" col-12 col-md-6 col-lg-4">
